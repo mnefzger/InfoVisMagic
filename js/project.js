6fc297eb563cbf3552fde4ae744c1f766200447f
mnefzger <matthiasnefzger.mail@gmail.com>
2014-12-15 14:54:11 +0100

    mittelguter fortschritt

--- js/project.js
+++ js/project.js
@@ -1,6 +1,25 @@
 	var content;
 	var papers = new Array();
 	var authors = new Array();
+	/*var HEIGHT = window.innerHeight;
+	var WIDTH = window.innerWidth;*/
+	var HEIGHT = 800;
+	var WIDTH = 1000;
+
+	var pixels = [];	
+	for(var y=0; y<HEIGHT; y++){
+		pixels[y] = [];
+		for(var x=0; x<WIDTH; x++){
+			pixels[y].push(0);
+		}
+	}
+	
+
+	var x_cor = 0;
+	var y_cor = 0;
+	var item_width;
+	var item_height;
+
 
 	$(document).ready(function() {
 
@@ -17,7 +36,6 @@
 	 });
 
 	function success(data){
-		console.log(data);
 		content = data;
 
 		$(".loader").hide();
@@ -40,7 +58,7 @@
 			}
 		});
 		//console.log(papers);
-		console.log(authors);
+		//console.log(authors);
 		makeItHappen();
 	};
 
@@ -69,16 +87,51 @@
 		return aut;
 	};
 
+	var calcPosition = function(w,h){		
+		outer: 
+		for(var y=10; y<pixels.length; y++){
+			for(var x=0; x<pixels[y].length; x++){
+				if(pixels[y][x] == 0){
+					x_cor = x;
+					y_cor = y;					
+					setTrue(x,y,w,h);	
+					break outer;			
+				}
+			}
+		}
+	}
+
+	var setTrue = function(X,Y,w,h){
+		console.log(w,h);
+		for(var y=Y; y<Y+h; y++){
+			for(var x=X; x<X+w; x++){
+				if(pixels[y] != undefined)	pixels[y][x] = 1;
+			}
+		}
+	}
+
+	var getX = function(){
+		return x_cor;
+	}
+	var getY = function(){
+		return y_cor;
+	}
+
 	var makeItHappen =  function(){
 		var data = [];
 		for(i=0;i<authors.length; i++){
-	    	data.push(authors[i].author);
+	    	data.push({author: authors[i].author});
 		}  
+		for(i=0;i<data.length; i++){
+			calcPosition(data[i].author.length*7, 12);
+			data[i]['x'] = getX();
+			data[i]['y'] = getY();
+		}
 
 		var container = d3.select('#container')
 			.append('svg')
-			.attr('width', window.innerWidth)
-			.attr('height', window.innerHeight);
+			.attr('width', WIDTH)
+			.attr('height', HEIGHT);
 
 		var items = container.selectAll('rect');
 		/*items
@@ -101,21 +154,29 @@
 	            .transition()
 	            .style( 'fill', 'white' );
 	        } );*/
-
+		
 
 	    items
 			.data(data)
 			.enter().append('text')
-			.text(function(d,i){return data[i]})
-	        .attr("x", function(d,i){return (i%14)*100;})
-	        .attr("y", function(d,i){return Math.floor(i/14)*20 + 50})
-	        .style("opacity",0)
-	        .style("font-size", function(d,i){
+			.text(function(d,i){return data[i].author})
+			.style("font-size", function(d,i){
 	        	var count = authors[i].papers.length;
-	        	if(count > 18) return authors[i].papers.length/3 +'px';
-	        	return '9px';
+	        	var fontSize;
+	        	if(count > 18) fontSize = authors[i].papers.length/3;
+	        	else fontSize = 9;
+	        	//return fontSize + 'px';
+	        	return '12px';
 	        })
-	        .style('fill', function(d,i){return randomColor()});
+	        .attr("x", function(d,i){
+	        	console.log(data[i].x)
+	        	return data[i].x;
+	        })
+	        .attr("y", function(d,i){
+	        	return data[i].y;
+	        })
+	        .style("opacity",0);
+	        //.style('fill', function(d,i){return randomColor()});
 
 		container.selectAll('rect')
 			.transition()
@@ -130,6 +191,11 @@
 			.delay(function(d,i){return i*10})
 			.each("start", function() { d3.select(this).style("opacity", "0"); })
 			.ease("linear")
-			.style("opacity", 1);
+			.style("opacity", function(d,i){
+				return authors[i].papers.length/10;
+			});
+			/*.style("font-size", function(d,i){
+				return authors[i].papers.length;
+			});*/
 			    
 	}
